50f6ceed0d912204987161816171690b52763439
SvenomOne <SSchaklewski@O365.comhard.de>
2024-09-27 14:57:53 +0200

    Add loggin in PHP file

--- todo-api.php
+++ todo-api.php
@@ -3,6 +3,16 @@
 header("Content-Type: application/josn");
 
 
+//LOG function in PHP
+function write_log($action, $data) {
+    $log = fopen ('log.txt', 'a');
+    $timestamp = date('Y-m-d H:i:s');
+    fwrite($log, "timestamp - $action: " . json_decode($data) . "\n");
+    fclose(($log));
+    
+}
+
+
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
         //Get Todo,s (READ)
@@ -15,6 +25,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         break;
     case "DELETE":
         //Remove Todo (DELETE)
+        write_log("DELETE", null);
         break;
 
 }
